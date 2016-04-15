ae4b0cb4b2bb65c99441485e486dfe21461748dc
Andrzej Rehmann <andrzej.rehmann@motorolasolutions.com>
2016-04-15 14:40:01 +0200

    setting aws cli autocomplete for commands in bash

--- deployment/install/install-aws-cli.sh
+++ deployment/install/install-aws-cli.sh
@@ -3,3 +3,6 @@ echo "[INFO] Installing AWS CLI..."
 curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
 unzip awscli-bundle.zip
 sudo ./awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
+
+# AWS command completion http://docs.aws.amazon.com/cli/latest/userguide/cli-command-completion.html
+echo "complete -C '/usr/local/aws/bin/aws_completer' aws" >> ~/.bash_profile
